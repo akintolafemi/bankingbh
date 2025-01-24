304702be15171d1803a217e55f4835bd3496c45b
Micheal <michealakintola106.pog@gmail.com>
2025-01-24 13:58:09 +0100

    Added the following 1. Swagger openapi for api documentation 2. seed data for database 3. Added constraint to reject multiple current account creation for customer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestApplication, NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppOptions, ValidationPipesOptions } from './app.utils';
@@ -17,6 +18,22 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe(ValidationPipesOptions));
 
   const logger = new Logger(NestApplication.name);
+
+  const config = new DocumentBuilder()
+    .addSecurity('bearer', {
+      scheme: 'bearer',
+      type: 'http',
+      bearerFormat: 'JWT',
+      name: 'authoriz',
+    })
+    .setTitle('BankingBH Accounts API')
+    .setDescription(
+      'API to be used for opening a new "current account" of already existing customers.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
 
   const port = process.env.PORT;
   await app.listen(port, () => {
